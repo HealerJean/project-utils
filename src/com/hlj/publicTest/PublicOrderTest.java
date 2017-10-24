0f41824e1eb7b068a085df17bce1d9c0bf5847a1
HealerJean123 <mxzdheaelr@gmail.com>
2017-10-24 10:08:40 +0800

    pulibc

---
+++ src/com/hlj/publicTest/PublicOrderTest.java
@@ -0,0 +1,18 @@
+package com.hlj.publicTest;
+/** 
+ * @author 作者 HealerJean
+ * @version 创建时间：2017年10月23日 下午2:39:46 
+ * 类说明 
+ */
+
+public class PublicOrderTest {
+	
+}
+
+
+ class A{
+	 //不能够运行
+	 public static void main(String[] args) {
+			System.out.println("123");
+		}
+}
