4f5f1d45c5dd749acf959999fc963ea6a9b7204b
HealerJean <healerjean@gmail.com>
2018-11-26 18:26:39 +0800

    hashMap

--- src/com/hlj/map/MapTest.java
+++ src/com/hlj/map/MapTest.java
@@ -1,9 +1,12 @@
 package com.hlj.map;
 
+import org.junit.Test;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Desc:
@@ -13,31 +16,8 @@ import java.util.TreeMap;
 public class MapTest {
 
 
-    public static Map<Long,Long> getStartIdAndEndId(Long count, Long fenduan){
-        Map<Long,Long> map = new HashMap<>();
-        Long num = count / fenduan;
-        Long yushu = count % fenduan;
-
-        for (int i = 1; i <= num; i++) {
-            map.put((i - 1) * fenduan + 1, i * fenduan);
-        }
-
-        Long yushufinal = num * fenduan + yushu;
-        map.put(num * fenduan + 1, yushufinal);
-
-        return map ;
+    @Test
+    public void testHashMap(){
     }
 
-
-    public static void main(String[] args) {
-
-
-        Map<Long, Long> tMapRever = new TreeMap<>(Collections.reverseOrder());
-        tMapRever.putAll(getStartIdAndEndId(31L,3L));
-
-
-        for(Long key :tMapRever.keySet() ){
-            System.out.println("key:"+key+":"+tMapRever.get(key));
-        }
-    }
 }
